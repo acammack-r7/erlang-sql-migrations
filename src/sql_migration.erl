6c5e5830dc105a68c64aa19c787758abcabfa0ae
Adam Cammack <adam_cammack@rapid7.com>
2016-09-05 19:28:48 -0500

    Modernize and switch to epgsql

--- src/sql_migration.erl
+++ src/sql_migration.erl
@@ -1,14 +1,9 @@
 -module(sql_migration).
 
--export([behaviour_info/1]).
-
 -export([migrations/1, migrate/3]).
 
-behaviour_info(callbacks) ->
-    [{upgrade,1},
-     {downgrade, 1}];
-behaviour_info(_Other) ->
-    undefined.
+-callback upgrade(any()) -> ok.
+-callback downgrade(any()) -> ok.
 
 migrations(App) ->
     {ok, Ms} = application:get_key(App, modules),
@@ -18,8 +13,8 @@ migrations(App) ->
     lists:usort(Migrations).
 
 migrate(Conn, Version, Migrations) ->
-    BinVersion = atom_to_binary(Version, latin1),
-    case pgsql:squery(Conn, "SELECT id FROM migrations ORDER BY id DESC") of
+    BinVersion = atom_to_binary(Version, unicode),
+    case epgsql:squery(Conn, "SELECT id FROM migrations ORDER BY id DESC") of
         {error,{error,error,<<"42P01">>,_,_}} ->
             %% init migrations and restart
             init_migrations(Conn),
@@ -28,34 +23,34 @@ migrate(Conn, Version, Migrations) ->
             up_to_date;
         {ok, _, [{Top}|_]} when Top < BinVersion ->
             %% upgrade path
-            TopAtom = binary_to_atom(Top, latin1),
+            TopAtom = binary_to_existing_atom(Top, unicode),
             Upgrade = lists:dropwhile(fun (V) -> V =< TopAtom end, Migrations),
-            [ begin 
+            [ begin
                   M:upgrade(Conn),
-                  pgsql:equery(Conn,
+                  epgsql:equery(Conn,
                                "INSERT INTO migrations (id) "
-                               "VALUES ($1)", [atom_to_binary(M, latin1)])
+                               "VALUES ($1)", [atom_to_binary(M, unicode)])
               end || M <- Upgrade ],
             {upgrade, Upgrade};
         {ok, _, [{Top}|_]} when Top > BinVersion ->
             %% downgrade path
-            TopAtom = binary_to_atom(Top, latin1),
+            TopAtom = binary_to_existing_atom(Top, unicode),
             Downgrade = lists:takewhile(fun (V) -> V >= TopAtom end, lists:reverse(Migrations)),
-            [ begin 
+            [ begin
                   M:downgrade(Conn),
-                  pgsql:equery(Conn,
+                  epgsql:equery(Conn,
                                "DELETE FROM migrations WHERE id = $1",
-                               [atom_to_binary(M, latin1)])
+                               [atom_to_binary(M, unicode)])
               end || M <- Downgrade ],
             {downgrade, Downgrade};
         {ok, _, []} ->
             %% full upgrade path
             Upgrade = Migrations,
-            [ begin 
+            [ begin
                   M:upgrade(Conn),
-                  pgsql:equery(Conn,
+                  epgsql:equery(Conn,
                                "INSERT INTO migrations (id) "
-                               "VALUES ($1)", [atom_to_binary(M, latin1)])
+                               "VALUES ($1)", [atom_to_binary(M, unicode)])
               end || M <- Upgrade ],
             {upgrade, Upgrade}
     end.
@@ -63,7 +58,7 @@ migrate(Conn, Version, Migrations) ->
 
 %% Private
 init_migrations(Conn) ->
-    {ok, _, _} = pgsql:squery(Conn,
+    {ok, _, _} = epgsql:squery(Conn,
                               "CREATE TABLE migrations ("
                               "id VARCHAR(255) PRIMARY KEY,"
                               "datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
